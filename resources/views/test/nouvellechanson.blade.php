add3c96691c80df1432ecae2692c01883e923ff2
Alexis <alexis.delanghe@gmail.com>
2022-03-27 09:29:37 +0200

    nouvelle chanson

--- resources/views/test/nouvellechanson.blade.php
+++ resources/views/test/nouvellechanson.blade.php
@@ -10,17 +10,22 @@
         </ul>
     </div>
 @endif
-<form method="post" action="/chanson/store" enctype="multipart/form-data">
+<form method="post" action="/chanson/store" enctype="multipart/form-data" 
+class="formulaire">
     @csrf
-    <input type="text" required placeholder="Title" name="title" value="{{old('title')}}"/>
+    <h1 class="nouvelle">Nouvelle Chanson</h1>
+    <input type="text" required placeholder="Title" name="title" value="{{old('title')}}"
+    class="titre"/>
     <br>
-    <input type="file" required  name="song"/>
+    <h2 class="fichier">Fichier</h2>
+    <input type="file" required  name="song" class="file"/>
     <br>
-    <label for="thumbnail">Ajouter une miniature</label>
-    <input type="file" required  name="thumbnail"/>
+    <label for="thumbnail" class="miniature">Ajouter une miniature</label><br>
+    <input type="file"  class="ajout-miniature" required  name="thumbnail"/>
     <br>
-    <input type="number" required min="0" max="10" placeholder='notes' value="{{old('note')}}" name="note"/>
+    <h2 class="note1">Note</h2>
+    <input type="number" class="note" required min="0" max="10" placeholder='notes' value="{{old('note')}}" name="note"/>
     <br>
-    <input type="submit" />
+    <input type="submit" class="submit"/>
 </form>
 @endsection
